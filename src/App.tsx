61529074c3b7f6c6a1a68cbd607b708e4988e51d
folafunmi-db <folafunmimustapha@gmail.com>
2020-10-22 15:29:39 +0100

    Added material icons and arranged items on the app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import "./App.css";
-import {Header} from "./"
+import Header from "./components/Header/header.component";
 
 function App() {
 	return (
 		<div className="App">
-      <Header />
+			<Header />
 			<Button variant="contained" color="primary">
 				Hello World
 			</Button>
--- src/components/Header/header.component.tsx
+++ src/components/Header/header.component.tsx
@@ -15,10 +15,59 @@ import {
 	AccountCircle,
 	More as MoreIcon,
 } from "@material-ui/icons";
+import {
+	fade,
+	makeStyles,
+	Theme,
+	createStyles,
+} from "@material-ui/core/styles";
 
 interface Props {}
 
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		search: {
+			position: "relative",
+			borderRadius: theme.shape.borderRadius,
+			backgroundColor: fade(theme.palette.common.white, 0.15),
+			"&:hover": {
+				backgroundColor: fade(theme.palette.common.white, 0.25),
+			},
+			marginRight: theme.spacing(2),
+			marginLeft: 0,
+			width: "100%",
+			[theme.breakpoints.up("sm")]: {
+				marginLeft: theme.spacing(3),
+				width: "auto",
+			},
+		},
+		searchIcon: {
+			padding: theme.spacing(0, 2),
+			height: "100%",
+			position: "absolute",
+			pointerEvents: "none",
+			display: "flex",
+			alignItems: "center",
+			justifyContent: "center",
+		},
+		inputRoot: {
+			color: "inherit",
+		},
+		inputInput: {
+			padding: theme.spacing(1, 1, 1, 0),
+			// vertical padding + font size from searchIcon
+			paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+			transition: theme.transitions.create("width"),
+			width: "100%",
+			[theme.breakpoints.up("md")]: {
+				width: "20ch",
+			},
+		},
+	})
+);
+
 export default function Header({}: Props): ReactElement {
+	const styles = useStyles();
 	return (
 		<AppBar position="static">
 			<Toolbar>
@@ -30,18 +79,22 @@ export default function Header({}: Props): ReactElement {
 					<MenuIcon />
 				</IconButton>
 				<Typography variant="h6" noWrap>
-					Material-UI
+					Movie Store
 				</Typography>
-				<div>
-					<div>
+				<div className={styles.search}>
+					<div className={styles.searchIcon}>
 						<SearchIcon />
 					</div>
 					<InputBase
 						placeholder="Search…"
+						classes={{
+                root: styles.inputRoot,
+                input: styles.inputInput,
+              }}
 						inputProps={{ "aria-label": "search" }}
 					/>
 				</div>
-				<div />
+				<div style={{ flexGrow: 1 }} />
 				<div>
 					<IconButton aria-label="show 4 new mails" color="inherit">
 						<Badge badgeContent={4} color="secondary">
