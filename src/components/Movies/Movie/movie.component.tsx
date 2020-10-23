c8868a8732bb1d48457d1d7b61c7fed1916c3415
folafunmi-db <folafunmimustapha@gmail.com>
2020-10-23 12:13:37 +0100

    Fetched movie data from imdb and displayed them\

--- src/components/Movies/Movie/movie.component.tsx
+++ src/components/Movies/Movie/movie.component.tsx
@@ -1,7 +1,18 @@
 import React from "react";
-
-interface Props {}
+import "./movie.style.css";
+interface Props {
+	title: string;
+	year: string;
+	image: string;
+}
 
 export const Movie = (props: Props) => {
-	return <div></div>;
+	console.log(props);
+	return (
+		<div className="movie">
+			<h2>{props.title}</h2>
+			<img src={props.image} alt={`${props.title} movie`} />
+			<h3>{props.year}</h3>
+		</div>
+	);
 };
--- src/components/Movies/movies.component.tsx
+++ src/components/Movies/movies.component.tsx
@@ -1,22 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Movie } from "./Movie/movie.component";
+import "./movies.style.css";
 
-interface Props {}
+// interface Props {
+// 	[x: string]: any;
+// 	movie: Movie;
+// }
+
+// interface Movie {
+// 	imdbID: string;
+// 	title: string[];
+// 	year: number;
+// 	image: any;
+// }
 
 const series = ["avengers", "fast and furious", "iron man", "harry potter"];
 
 // imdb api free key
 const API_KEY = "95b26974";
 
-export const Movies = (props: Props) => {
+export const Movies = () => {
+	const [movies, setMovies] = useState<any>([]);
 	useEffect(() => {
-		series.forEach((series) => {
+		const promises = series.map((series) => {
 			fetch(
-				`http://www.omdbapi.com/?apikey=${API_KEY}&s=${encodeURIComponent(series)}&page=1`
-			)
-				.then((res) => res.json())
-				.then((res) => console.log(res));
+				`http://www.omdbapi.com/?apikey=${API_KEY}&s=${encodeURIComponent(
+					series
+				)}&page=1`
+			).then((res) => res.json());
 		});
+
+		// After all promises get resolved
+		Promise.all(promises).then((movies) => setMovies(movies));
 	}, []);
 
-	return <div>a</div>;
+	console.log(movies);
+
+	return (
+		<div className="movies">
+			{movies.flat(2).map((movie: any) => {
+				return (
+					<Movie
+						key={movie.imdbID}
+						title={movie.title}
+						year={movie.year}
+						image={movie.image}
+					/>
+				);
+			})}
+		</div>
+	);
 };
